78767439c66ff7be629b3482d0f52989a3ed358c
buckster12 <roboks@mail.ru>
2018-11-04 13:41:56 +0300

    Initial Commit

---
+++ Taskun/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Taskun
+//
+//  Created by Dzmitry Harbatsevich on 11/4/18.
+//  Copyright © 2018 None, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Taskun/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Taskun
+//
+//  Created by Dzmitry Harbatsevich on 11/4/18.
+//  Copyright © 2018 None, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
